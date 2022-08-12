8e4db809f0b06790b2245bab63a3655920b58279
D3troit98 <duruakuebuka@gmail.com>
2022-08-12 03:13:27 +0100

    a wroking terms form

--- source/policifyProject/settings.py
+++ source/policifyProject/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'policifyProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE':'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'policify_v2',
-        #'USER': 'postgres', 
-        #'PASSWORD': 'nothingmore',
-        #'HOST': '127.0.0.1', 
-        #'PORT': '5432',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'policify_v2',
+        'USER': 'postgres', 
+        'PASSWORD': 'nothingmore',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
         
         #comment my own when you want to start
 
@@ -148,10 +148,12 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = "/dashboard"
 LOGOUT_REDIRECT_URL = "/home"
 
+#SMTP Configuration
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='duruakuebuka@gmail.com'
-EMAIL_HOST_PASSWORD = 'transmission'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '*********'
+EMAIL_HOST_PASSWORD = '*********'
 
