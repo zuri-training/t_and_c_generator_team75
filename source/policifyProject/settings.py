3fa7060545d07550c3964ddf395488545a49173e
D3troit98 <duruakuebuka@gmail.com>
2022-08-12 01:24:19 +0100

    password reset

--- source/policifyProject/settings.py
+++ source/policifyProject/settings.py
@@ -13,6 +13,7 @@ from email.policy import default
 from decouple import config
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +142,11 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = "/dashboard"
 LOGOUT_REDIRECT_URL = "/home"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='duruakuebuka@gmail.com'
+EMAIL_HOST_PASSWORD = 'transmission'
+
