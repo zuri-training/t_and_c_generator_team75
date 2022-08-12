678d874d0ed050775aee8c22d85f3805cbd57149
Abby <harbeyteejay@gmail.com>
2022-08-12 23:33:26 +0100

    buttons

--- source/policifyapps/urls.py
+++ source/policifyapps/urls.py
@@ -3,7 +3,7 @@ from .views import HomePageView, FeedbackPageView, DashboardPageView
 from django.contrib.auth import views
 from .views import sign_up , create_policiy_post , create_terms_post , all_post , preview_post , edit_post ,GeneratePdf
 
-from .views import HomePageView, FeedbackPageView, DashboardPageView, PrivacyDashboardPageView, TermsDashboardPageView, PolicyPreviewPageView, ProductPageView, ContactPageView, TermsPreviewPageView
+from .views import HomePageView, FeedbackPageView, DashboardPageView, PrivacyDashboardPageView, TermsDashboardPageView, PolicyPreviewPageView, ProductPageView, ContactPageView, TermsPreviewPageView, PPPreviewPageView, TCPreviewPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
@@ -16,6 +16,8 @@ urlpatterns = [
     #path("termsdashboard/", TermsDashboardPageView.as_view(), name="termsdashboard"),
     #path("policypreview/", PolicyPreviewPageView.as_view(), name="policypreview"),
     path("termspreview/", TermsPreviewPageView.as_view(), name="policypreview"),
+    path("tcpreview/", TCPreviewPageView.as_view(), name="tcpreview"),
+    path("pppreview/", PPPreviewPageView.as_view(), name="pppreview"),
     
     path("privacypolicydashboard/", create_policiy_post, name="privacypolicydashboard"),
     path("termsdashboard/", create_terms_post, name="termsdashboard"),
