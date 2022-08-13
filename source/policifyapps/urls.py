d8d5ac354df134101d16fab190f18014f821271f
D3troit98 <duruakuebuka@gmail.com>
2022-08-13 09:23:13 +0100

    Everything is done!

--- source/policifyapps/urls.py
+++ source/policifyapps/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import HomePageView, FeedbackPageView, DashboardPageView
 from django.contrib.auth import views
-from .views import sign_up , create_policiy_post , create_terms_post , all_post , preview_post , edit_post ,GeneratePdf ,doc_view , policies
+from .views import sign_up , create_policiy_post , create_terms_post , all_post , preview_post , edit_post ,GeneratePdf ,doc_view , policies,delete_post
 
 from .views import HomePageView, FeedbackPageView, DashboardPageView, PrivacyDashboardPageView, TermsDashboardPageView, PolicyPreviewPageView, ProductPageView, ContactPageView, TermsPreviewPageView, PPPreviewPageView, TCPreviewPageView
 
@@ -24,13 +24,17 @@ urlpatterns = [
     path('sign-up',sign_up,name='sign_up'),
     path('dashboard/',all_post,name='dashboard'),
     path('policypreview/<int:post_type>/<int:my_id>',preview_post,name='preview_post'),
-     path('edit-post/<int:my_id>',edit_post,name='edit_post'),
+    path('dashboard/policypreview/<int:post_type>/<int:my_id>',preview_post,name='preview_post'),
+     path('edit-post/<int:post_type>/<int:my_id>',edit_post,name='edit_post'),
+     path('dashboard/edit-post/<int:post_type>/<int:my_id>',edit_post,name='edit_post'),
      path('pdf/<int:post_type>/<int:my_id>', GeneratePdf.as_view()),
      path('dashboard/termsdashboard/',create_terms_post, name="termsdashboard"),
      path("dashboard/privacypolicydashboard/", create_policiy_post, name="privacypolicydashboard"),
      path('word-post/<int:post_type>/<int:my_id>',doc_view,name='word_post'),
       path('policies',policies,name='policies'),
         path('dashboard/policies',policies,name='policies'),
+     path('delete-post/<int:post_type>/<int:my_id>',delete_post,name='delete_post'),
+       path('dashboard/delete-post/<int:post_type>/<int:my_id>',delete_post,name='delete_post'),
 
 
 
