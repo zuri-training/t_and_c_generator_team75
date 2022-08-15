a49219005e9bb76ee9fcb4db791d9ab3dd8fae1c
Abby <harbeyteejay@gmail.com>
2022-08-15 13:20:24 +0100

    changes

--- source/policifyProject/settings.py
+++ source/policifyProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')#, default='django-insecure-y5nd&=v=yo(r55um!99ps^i+06(8189j4j5=o6%mor0zje7)^d')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-y5nd&=v=yo(r55um!99ps^i+06(8189j4j5=o6%mor0zje7)^d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
